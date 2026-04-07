bfd746a3703dc9a672c3a9b44276c1f125dbe8a3
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2026-04-07 09:59:39 -0600

    added comments and cleaned up code. Updated directory for the final raster

--- scripts/final/01_archetype_analysis_for_depend_prep.R
+++ scripts/final/01_archetype_analysis_for_depend_prep.R
@@ -1,3 +1,20 @@
+################################################################################
+# SCRIPT TO DOWNLOAD AND PROCESS FORESTRY DEPENDENCE DATA TO 3KM              ##
+# 1. Load the data                                                            ##
+#  1.1 Filter and select variables                                            ##
+#  1.2 This is county level data so need to download county boundaries from   ##
+#      tigris using the year 2020                                             ##
+# 2. Join the forest dependence data to the county geometeries                ##
+#  2.1 Check that the new geometries are valid and not empty                  ##
+# 3. Fill in the missing data                                                 ##
+#  3.1 Create an empty raster with 3km resolution                             ##
+#  3.2 Use a custom function to interpolate the missing data                  ##
+# 4. Crop and mask the raster prediction to the ref_raster                    ##
+# 5. Save the raster                                                          ##
+################################################################################
+
+# 0. Load the required libraries
+#-------------------------------------------------------------------------------
 library(stringr)
 library(sf)
 library(terra)
@@ -12,13 +29,28 @@ library(stars)
 # Set the projection
 projection <- "epsg:5070"
 
-# Load the data
+# Load the reference raster
+ref_rast <- rast("/Users/katiemurenbeeld/Analysis/SES_Forest_Archetypes/data/processed/variables/conus_whp_3km_agg_interp_crop_2024-09-27.tif")
+
+# Read in the custom functions
+source(here::here("scripts/functions/custom_functions.R"))
+
+
+# 1. Load the data
+#-------------------------------------------------------------------------------
+# Forest dependency data can be found here:
+# https://www.fs.usda.gov/rds/archive/products/RDS-2021-0077/RDS-2021-0077.zip
+
 forest_depend <- read_csv("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/original/Data/County-Forest_Dep_Comm_Capital.csv")
 
-# Load the reference raster and reproject
-ref_rast <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/merged/conus_whp_3km_agg_interp_crop_2024-09-27.tif")
-ref_rast_proj <- project(ref_rast, projection)
+## 1.1
+## Forest Dependence Variables: select the % forest pay and rename fips to FIPS
+fordep <- forest_depend %>% # needs 2020 counties
+  dplyr::select(fips, pct.pay) %>%
+  rename("FIPS" = "fips", "pct_forpay" = "pct.pay")
 
+
+## 1.2
 ## Load county boundaries from tigris
 ### Get Continental US list
 us.abbr <- unique(fips_codes$state)[1:51]
@@ -37,25 +69,19 @@ counties_2020 <- counties_2020 %>%
   filter(STATEFP %in% continental.states$FIPS) %>%
   dplyr::select(GEOID, geometry)
 
-## Forest Dependence Variables: select the % forest pay
-fordep <- forest_depend %>% # needs 2020 counties
-  dplyr::select(fips, pct.pay) %>%
-  rename("FIPS" = "fips", "pct_forpay" = "pct.pay")
-
-## Make sure all FIPS codes are padded with 0s for a total of 5 characters
-update_fips <- function(data_set) {
-  data_set$FIPS <- as.character(data_set$FIPS)
-  data_set$FIPS <- str_pad(data_set$FIPS, 5, side="left", pad="0")
-  return(data_set)
-}
+## Use the update_fips function to pad all FIPS codes in your data with 0s for
+## a total of 5 characters 
 
 fordep_fips <- update_fips(fordep)
 
+# 2. Join the data to the county geometries
+#-------------------------------------------------------------------------------
 ## join to the 2020 counties
 fordep_county <- left_join(counties_2020, fordep_fips,
                       by = c("GEOID" = "FIPS"))
 
-## check for validity, remove empty geometries, and reproject 
+## 2.1
+## Check for validity, remove empty geometries, and reproject 
 if (!all(st_is_valid(fordep_county)))
   fordep_county <- st_make_valid(fordep_county)
 
@@ -65,7 +91,9 @@ fordep_county <- fordep_county %>%
 fordep_proj <- fordep_county %>%
   st_transform(projection)
 
-## Create a template raster for the shapefiles
+# 3. Fill in missing data
+#-------------------------------------------------------------------------------
+## 3.1 Create a template raster for the shapefiles
 XMIN <- ext(ref_rast_proj)$xmin
 XMAX <- ext(ref_rast_proj)$xmax
 YMIN <- ext(ref_rast_proj)$ymin
@@ -80,27 +108,19 @@ templateRas <- rast(ncol=NCOLS, nrow=NROWS,
 
 grd <- st_as_stars(templateRas)
 
-# function to rasterize variable, make points, make predictions
-# raster_grid is like a reference raster
-idw_preds <- function(data_proj, ref_raster, lay, empty_grid){
-  var.rst <- rasterize(data_proj, ref_raster, field = lay, fun = "mean")
-  var.pt <- as.points(var.rst) %>%
-    st_as_sf(.)
-  var.pred <- idw(var.pt[[1]]~1, var.pt, empty_grid)
-  var.pred.rst <- rasterize(st_as_sf(var.pred), ref_raster, field = "var1.pred")
-  names(var.pred.rst) <- paste0(lay, ".pred")
-  var.pred.rst.crop <- crop(var.pred.rst, ref_raster, mask = TRUE)
-  #return(var.pred.rst)
-  return(c(orig.rst = var.rst, pred.rst = var.pred.rst.crop))
-}
-
+## 3.2 Use the idw_preds function to rasterize and fill in missing data using 
+##     inverse distance weigthing (idw)
 fordep.preds <- idw_preds(fordep_proj, templateRas, "pct_forpay", grd)
-plot(ref_rast_proj)
-plot(fordep.preds$orig.rst)
-plot(fordep.preds$pred.rst)
-# didn't crop properly in function
+
+# 4. Crop to reference raster
+#-------------------------------------------------------------------------------
 forpred_crop <- crop(fordep.preds$pred.rst, ref_rast_proj, mask = TRUE)
+
+# quick check that it cropped
 plot(forpred_crop)
-writeRaster(forpred_crop, paste0("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/pct_forpay_3km_pred_crop_", 
+
+# 5. Save the raster
+#-------------------------------------------------------------------------------
+writeRaster(forpred_crop, paste0("/Users/katiemurenbeeld/Analysis/SES_Forest_Archetypes/data/processed/variables/pct_forpay_3km_pred_crop_", 
                                          Sys.Date(), ".tif"), overwrite = TRUE)
 
