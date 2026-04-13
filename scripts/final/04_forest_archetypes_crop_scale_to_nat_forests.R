7f9c7e1c6eb0277d7fa0cd9c5d8248623ab388b2
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2026-04-13 16:22:31 -0600

    new script to crop the raster stack to the national forest + 50km buffer. Still need to update comments and file paths

---
+++ scripts/final/04_forest_archetypes_crop_scale_to_nat_forests.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+library(terra)
+library(geocmeans)
+library(sf)
+library(here)
+library(viridis)
+
+# 1. Load the attribute raster stack and forest + buffer shape file
+
+## Attribute raster stack
+df_attri <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/rast_stack_all_attributes_2024-10-08.tif")
+
+
+## Have to crop to CONUS. Load in regional USFS boundaries.
+## Load the USFS boundaries
+fs_reg <- st_read("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/original/S_USA.AdministrativeRegion.shp")
+
+## Set the projection
+projection <- "epsg: 5070"
+
+## Filter out Region 10 and crop to the raster stack
+fs_reg.proj <- fs_reg %>% 
+  filter(REGION != "10") %>%
+  st_transform(., crs=projection)
+fs_reg.crop <- st_crop(fs_reg.proj, ext(df_attri))
+
+## create a national forest buffers shape using st_intersection
+nf_buffers <- st_intersection(nf_sf, fs_reg.crop)
+
+# 2.Crop the raster to the nf_buffers. Then scale using global min-max.
+## Because this is cropped. I should look at the variace of the values. 
+## the z-score may be appropriate once restricted to the forest buffers.
+
+attri_crop <- crop(df_attri, nf_buffers, mask = TRUE)
+attri_crop_sc <- (attri_crop - global(attri_crop, "min", na.rm=TRUE)[,1])/(global(attri_crop, "max", na.rm=TRUE)[,1] - global(attri_crop, "min", na.rm=TRUE)[,1])
+
+## I'M NOT SURE IS THIS IS APPROPRIATE OR NOT ##
+attri_crop_sc[is.na(attri_crop_sc)] <- -999 
+plot(attri_crop_sc$aip)
+
+## save for the next scripts (this could be its own small script)
+writeRaster(attri_crop, filename = here::here(paste0("data/processed/nf_buffers_all_attributes_", 
+                                                     Sys.Date(), ".tif")))
+
+writeRaster(attri_crop_sc, filename = here::here(paste0("data/processed/nf_buffers_all_attributes_cropped_then_scaled_", 
+                                                        Sys.Date(), ".tif")), overwrite = TRUE)
