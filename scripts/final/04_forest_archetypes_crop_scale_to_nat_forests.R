c73e02529e471937702e3b8f66ac905729562a00
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2026-04-14 11:24:40 -0600

    started updating comments. need to rename

--- scripts/final/04_forest_archetypes_crop_scale_to_nat_forests.R
+++ scripts/final/04_forest_archetypes_crop_scale_to_nat_forests.R
@@ -1,3 +1,18 @@
+################################################################################
+# SCRIPT TO CROP THE RASTER STACK TO THE NATIONAL FOREST + 50KM BUFFERS       ##
+# 1. Load the UNSCALED full attribute raster stack and the USFS regional      ##
+#   boundaries                                                                ##
+#  1.1 Filter the regional boundaries (remove Region 10), reproject, and then ##
+#      crop the boundaries to extent of the raster stack                      ##
+# 2. Crop the raster stack to the reg ##
+#  k = 27, m = 1.2, alpha = 0.1, beta = 0.1, window size = 3x3                ##
+#  2.1 Save the model as an RDS                                               ##
+# 3. Extract and plot the raster of the cluster groups                        ##
+#  3.1 Save the cluster groups raster                                         ##
+################################################################################
+
+# 0. Load the libraries
+#-------------------------------------------------------------------------------
 library(tidyverse)
 library(terra)
 library(geocmeans)
@@ -5,19 +20,21 @@ library(sf)
 library(here)
 library(viridis)
 
-# 1. Load the attribute raster stack and forest + buffer shape file
-
-## Attribute raster stack
-df_attri <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/rast_stack_all_attributes_2024-10-08.tif")
-
-
-## Have to crop to CONUS. Load in regional USFS boundaries.
-## Load the USFS boundaries
-fs_reg <- st_read("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/original/S_USA.AdministrativeRegion.shp")
-
 ## Set the projection
 projection <- "epsg: 5070"
 
+# 1. Load the attribute raster stack and forest + buffer shape file
+#-------------------------------------------------------------------------------
+## Attribute raster stack
+df_attri <- rast("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/processed/rast_stack_all_attributes_2024-10-08.tif")
+
+## Load in forest with 50km buffer shape file
+nf_sf <- read_sf(here::here("data/processed/nf_buffers_50k_2024-10-22.shp"))
+
+## Load in regional USFS boundaries
+fs_reg <- st_read("/Users/katiemurenbeeld/Analysis/Archetype_Analysis/data/original/S_USA.AdministrativeRegion.shp")
+
+## 1.1
 ## Filter out Region 10 and crop to the raster stack
 fs_reg.proj <- fs_reg %>% 
   filter(REGION != "10") %>%
