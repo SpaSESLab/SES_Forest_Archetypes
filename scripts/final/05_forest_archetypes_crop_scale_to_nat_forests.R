7a0a3de232bd7218c55b1a2a0f1a37e36e8db933
Katie Murenbeeld <katiemurenbeeld@boisestate.edu>
2026-04-14 11:44:36 -0600

    Cleaned up code. Updated comments

--- scripts/final/05_forest_archetypes_crop_scale_to_nat_forests.R
+++ scripts/final/05_forest_archetypes_crop_scale_to_nat_forests.R
@@ -1,24 +1,22 @@
 ################################################################################
 # SCRIPT TO CROP THE RASTER STACK TO THE NATIONAL FOREST + 50KM BUFFERS       ##
-# 1. Load the UNSCALED full attribute raster stack and the USFS regional      ##
-#   boundaries                                                                ##
+# 1. Load the UNSCALED full attribute raster stack, the USFS regional boundaries
+#   and the national forest with buffers shapefile                            ##
 #  1.1 Filter the regional boundaries (remove Region 10), reproject, and then ##
 #      crop the boundaries to extent of the raster stack                      ##
-# 2. Crop the raster stack to the reg ##
-#  k = 27, m = 1.2, alpha = 0.1, beta = 0.1, window size = 3x3                ##
-#  2.1 Save the model as an RDS                                               ##
-# 3. Extract and plot the raster of the cluster groups                        ##
-#  3.1 Save the cluster groups raster                                         ##
+#  1.2 For some reason the original nf_buffers shapefile is not cropped to Conus
+#      Need to create a new national forest buffers shape using st_intersection#
+#  2. Crop the raster to the nf_buffers                                       ##
+#  2.1 Scale using global min-max.                                            ##
+# 3. Save the cropped and the cropped then scaled raster                      ##
 ################################################################################
 
 # 0. Load the libraries
 #-------------------------------------------------------------------------------
 library(tidyverse)
 library(terra)
-library(geocmeans)
 library(sf)
 library(here)
-library(viridis)
 
 ## Set the projection
 projection <- "epsg: 5070"
@@ -41,23 +39,32 @@ fs_reg.proj <- fs_reg %>%
   st_transform(., crs=projection)
 fs_reg.crop <- st_crop(fs_reg.proj, ext(df_attri))
 
-## create a national forest buffers shape using st_intersection
+## 1.2 
+## For some reason the original nf_buffers shapefile is not cropped to Conus
+## Need to create a new national forest buffers shape using st_intersection
 nf_buffers <- st_intersection(nf_sf, fs_reg.crop)
 
-# 2.Crop the raster to the nf_buffers. Then scale using global min-max.
-## Because this is cropped. I should look at the variace of the values. 
-## the z-score may be appropriate once restricted to the forest buffers.
-
+# 2. Crop the raster to the nf_buffers. Then scale using global min-max.
+#-------------------------------------------------------------------------------
 attri_crop <- crop(df_attri, nf_buffers, mask = TRUE)
+
+## 2.1
+## Scale using global min-max
 attri_crop_sc <- (attri_crop - global(attri_crop, "min", na.rm=TRUE)[,1])/(global(attri_crop, "max", na.rm=TRUE)[,1] - global(attri_crop, "min", na.rm=TRUE)[,1])
+## Because this is cropped. I should look at the variance of the values. 
+## the z-score may be appropriate once restricted to the forest buffers.
+## UPDATE: I tested this as well as rank scale. Global is still the most 
+## appropriate. 
 
-## I'M NOT SURE IS THIS IS APPROPRIATE OR NOT ##
-attri_crop_sc[is.na(attri_crop_sc)] <- -999 
-plot(attri_crop_sc$aip)
-
-## save for the next scripts (this could be its own small script)
-writeRaster(attri_crop, filename = here::here(paste0("data/processed/nf_buffers_all_attributes_", 
+# 3. Save for the cropped and the cropped than scaled raster stack
+#-------------------------------------------------------------------------------
+writeRaster(attri_crop, 
+            filename = here::here(paste0("data/processed/nf_buffers_all_attributes_", 
                                                      Sys.Date(), ".tif")))
 
-writeRaster(attri_crop_sc, filename = here::here(paste0("data/processed/nf_buffers_all_attributes_cropped_then_scaled_", 
+writeRaster(attri_crop_sc, 
+            filename = here::here(paste0("data/processed/nf_buffers_all_attributes_cropped_then_scaled_", 
                                                         Sys.Date(), ".tif")), overwrite = TRUE)
+
+
+
